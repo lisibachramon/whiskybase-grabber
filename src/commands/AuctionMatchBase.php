83e23acbaad5282e3a4c7f95408ae2ca2183b843
ramon <r.lisi>
2021-02-12 22:08:06 +0100

    just checking on the server

--- src/commands/AuctionMatchBase.php
+++ src/commands/AuctionMatchBase.php
@@ -32,60 +32,82 @@ class AuctionMatchBase extends Command
             // the "--help" option
             ->setHelp('This command allows you to be a wizard.');
     }
+
     //Don't try to understand this... Not even the author does..
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $db = new Db();
         $query = 'SELECT auction_id, name FROM whisky.auction ';
-        if (!empty($input->getArgument('auction'))){
-            $query .= "where auction = '".$input->getArgument('auction')."'";
+        if (!empty($input->getArgument('auction'))) {
+            $query .= "where auction = '" . $input->getArgument('auction') . "'";
         }
-        $query .=";";
+        $query .= ";";
         $res = $db->get($query);
         foreach ($res as $re) {
             //get correct strength
-            $data = $re['stength'] ;
-            $data = preg_replace('/[^0-9.]+/', '', $data);
-            $q = "SELECT whiskeybase_id FROM whisky.whiskeybase where  ";
+            $strength = $re['strength'];
+            $strength = preg_replace('/[^0-9.]+/', '', $strength);
+
+            $size = $re['size'];
+            $size = str_replace('cl', '0', $size);
+            $size = str_replace('ml', '00', $size);
+            $size = str_replace('l', '000', $size);
+            $size = str_replace('.', '', $size);
 
 
+            $q = "SELECT whiskeybase_id FROM whisky.whiskeybase where strength like '%$strength%' AND size = '$size' ";
+            $ids = $db->get($q);
+            $baseIdMatch = " AND (";
+            foreach ($ids as $id) {
+                $baseIdMatch .= "whiskeybase_id = " . $id['whiskeybase_id'] . " OR ";
+            }
+            $baseIdMatch .= ")";
+            $pos = strrpos($baseIdMatch, "OR");
 
+            if ($pos !== false) {
+                $baseIdMatch = substr_replace($baseIdMatch, "", $pos, strlen("OR"));
+            }
 
-            $concat = "CONCAT(name, bottler, vintage)";
-            $q = "SELECT whiskeybase_id FROM whisky.whiskeybase where $concat ";
-            $match = [];
-            if (strpos(strtolower($data), "miniature") === false) {
-                foreach ($darr as $d) {
-                    $l = $q . "like '%" . str_replace("'", "", $d) . "%'";
-                    $l .= ";";
-                    $res = $db->get($l);
+            $auctionLotName = $re['name'] ;
+            $auctionLotName = str_replace('years old', '', $auctionLotName);
+            $auctionLotName = str_replace('Years Old', '', $auctionLotName);
+            $auctionLotName = str_replace('Year Old', '', $auctionLotName);
+            $auctionLotName = str_replace('year old', '', $auctionLotName);
 
-                    foreach ($res as $r) {
+            $auctionLotNameArr = explode(" ", $auctionLotName);
 
-                        $id = $r["whiskeybase_id"];
-                        if (!array_key_exists($id, $match)) {
-                            $match[$id] = 1;
-                        } else {
-                            $match[$id] = $match[$id] + 1;
-                        }
+            $matches=[];
+            foreach ($auctionLotNameArr as $fragment)
+            {
+                $q = "SELECT whiskeybase_id FROM whisky.whiskeybase where CONCAT(name, bottler, vintage, serie, description) like '%$fragment%'" . $baseIdMatch;
+                $fragmentMatches = $db->get($q);
+                foreach ($fragmentMatches as $fm)
+                {
+                    $id = $fm['whiskeybase_id'];
+                    if (!array_key_exists($fm, $matches)) {
+                        $matches[$id] = 1;
+                    } else {
+                        $matches[$id] = $matches[$id] + 1;
                     }
+                }
 
+            }
+            $max = max($matches);
+            $count = 0;
+            foreach ($matches as $item) {
+                if ($item === $max) {
+                    $count++;
                 }
-                $max = max($match);
-                $count = 0;
-                foreach ($match as $item) {
-                    if ($item === $max) {
-                        $count++;
-                    }
-                }
-                if ($count == 1 && ($max / sizeof($darr)) >= 0.6) {
-                    echo "Accuracy: " . ($max / sizeof($darr)) . "\n";
+            }
+            print_r($matches);
+            die;
+            if ($count == 1 && ($max / sizeof($auctionLotNameArr)) >= 0.6) {
+                echo "Accuracy: " . ($max / sizeof($auctionLotNameArr)) . "\n";
 
-                    $db->insertMatch($re['auction_id'], array_search($max, $match), ($max / sizeof($darr)));
-                    // print_r(array_keys($match, max($match)));
-                } else {
-                    echo "no secure match \n";
-                }
+                //$db->insertMatch($re['auction_id'], array_search($max, $matches), ($max / sizeof($auctionLotNameArr)));
+                 print_r(array_keys($matches, max($matches)));
+            } else {
+                echo "no secure match \n";
             }
         }
     }
